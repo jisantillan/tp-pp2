98eb531e6be25d4de8658dd608605f7b1f45bcae
Kevops <kruiz@sysone.com>
2024-09-16 16:29:33 -0300

    refactor: refactor main

--- src/main/java/org/domingus/DomingusApp.java
+++ src/main/java/org/domingus/DomingusApp.java
@@ -7,13 +7,23 @@ import org.domingus.config.DomingusConfiguration;
 import org.domingus.polling.*;
 import java.io.*;
 
+import static java.util.Objects.isNull;
+
 public class DomingusApp {
 
     private static final String CONFIG_FILE_PROPERTY = "configFile";
 
     public static void main(String[] args) throws InterruptedException, IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        DomingusConfiguration config = mapper.readValue(System.getProperty(CONFIG_FILE_PROPERTY), DomingusConfiguration.class);
+        DomingusConfiguration config = null;
+        if(!(args.length == 0)) {
+            config = getConfigFromArgs(args[0]);
+        } else {
+            config = getDefaultConfig();
+        }
+
+        if(isNull(config)) {
+            throw new RuntimeException("There is not a configuration for the app");
+        }
 
         Notifier notifier = new Notifier();
 
@@ -22,19 +32,19 @@ public class DomingusApp {
 
         VersionManager versionsManager = new VersionManager(changeDetector);
 
-        DataFetcher dataFetcher = new DataFetcher(versionsManager, config.getDefaultData());
+        DataFetcher dataFetcher = new DataFetcher(versionsManager, config.getDefaultData().get(0));
 
         Timer timer = new Timer(config.getTimerInterval());
         timer.addObserver(dataFetcher);
         timer.start();
     }
 
-    private static DomingusConfiguration opcionDos() throws JsonProcessingException {
+    private static DomingusConfiguration getDefaultConfig() throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
         return mapper.readValue(System.getProperty(CONFIG_FILE_PROPERTY), DomingusConfiguration.class);
     }
 
-    private static DomingusConfiguration opcionUno(String arg) throws IOException {
+    private static DomingusConfiguration getConfigFromArgs(String arg) throws IOException {
         File file = new File(arg);
         ObjectMapper mapper = new ObjectMapper();
         return mapper.readValue(file, DomingusConfiguration.class);
