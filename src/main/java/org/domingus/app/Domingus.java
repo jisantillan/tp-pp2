128b15096da3ec75a0c0e42540d8c2e7ce40e44f
lc-sanchez <csanchez_98@outlook.com>
2024-10-06 22:01:28 -0300

    Removed config.json and managed empty path error

--- src/main/java/org/domingus/app/Domingus.java
+++ src/main/java/org/domingus/app/Domingus.java
@@ -9,15 +9,8 @@ import org.domingus.init.PlatformDiscoverer;
 import org.domingus.interfaces.NotificationPlatform;
 import org.domingus.interfaces.Source;
 
-
 public class Domingus {
-	
-    public static final String INITIALIZING_FAIL_MESSAGE = "There is not a configuration for the app";
-    public static final String DEFAULT_CONFIGURATION_PATH_FILE = "src\\main\\resources\\configuration\\configuration.json";
 
-    private Set<NotificationPlatform> platforms;
-    private PlatformDiscoverer discoverer;
-    
     private Notifier notifier;
     private Source source;
     
@@ -26,24 +19,21 @@ public class Domingus {
             this.notifier = new Notifier();
     }
 
-
     public void init(String extensionPath) throws InterruptedException, IOException, ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
-        discoverer = new PlatformDiscoverer();
-        this.platforms = discoverer.discover(extensionPath);
-        platforms.forEach(notifier::addPlatform);
-
+        if (extensionPath != null && !extensionPath.isEmpty()){
+            PlatformDiscoverer discoverer = new PlatformDiscoverer();
+            Set<NotificationPlatform> platforms = discoverer.discover(extensionPath);
+            platforms.forEach(notifier::addPlatform);
+        }
         MessageAdapter messageAdapter = new MessageAdapter(notifier);
         ChangeDetector changeDetector = new ChangeDetector(messageAdapter);
-
         if (source != null) {
             source.suscribe(changeDetector);
         }
-
     }
 
-
     public void addObserver(NotificationPlatform observer) {
-    	notifier.addPlatform(observer);
+        notifier.addPlatform(observer);
     }
     
 }
