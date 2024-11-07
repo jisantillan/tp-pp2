afd1dd5f803d494c027dccdcb8fbebb432b4a0fd
kruiz <kruiz@sysone.com>
2024-11-07 20:03:46 -0300

    feat(): receive timeInterval from args

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -5,22 +5,34 @@ import org.domingus.init.DomingusFactory;
 import org.domingus.interfaces.Observer;
 import org.domingus.interfaces.Source;
 
+import static java.util.Objects.nonNull;
+
 public class Main implements Observer {
 	
 	public static void main(String[] args) throws FileNotFoundException {
 		System.out.println("Iniciando DomingusApp");
-
 		Source sender = new SourceSender();
-		Timer timer = new Timer(2000, (Runnable) sender);
 
-		DomingusFactory factory = new DomingusFactory();
-		Domingus domingus = factory.create(sender, "src\\test\\resources\\extensions\\");
+		Integer timeInterval = null;
+		try	{
+			timeInterval = Integer.valueOf(args[0]);
+		} catch (ArrayIndexOutOfBoundsException | NumberFormatException e) {
+			System.out.println("Error obteniendo los segundos por parámetro");
+		}
 
-		Main main = new Main();
-		domingus.addObserver(main);
-		domingus.addCurrentObserver(main.getClass().getSimpleName());
+		if(nonNull(timeInterval)) {
+			Timer timer = new Timer(timeInterval, (Runnable) sender);
 
-		timer.run();
+			DomingusFactory factory = new DomingusFactory();
+			Domingus domingus = factory.create(sender, "src\\test\\resources\\extensions\\");
+
+			Main main = new Main();
+			domingus.addObserver(main);
+			domingus.addCurrentObserver(main.getClass().getSimpleName());
+
+			timer.run();
+		}
+
 	}
 
 	@Override
